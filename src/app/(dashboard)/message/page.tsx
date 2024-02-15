54c6a9b8adcc5f87094097af7860f6a776aa0915
amitsharma-hs01 <111582458+amitsharma-hs01@users.noreply.github.com>
2024-02-15 13:22:28 +0530

    initial commit

--- src/app/(dashboard)/message/page.tsx
+++ src/app/(dashboard)/message/page.tsx
@@ -1,6 +1,28 @@
 import Messages from "@/components/message/messages";
+import { supabaseClient } from "@/lib/supabaseClient";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function Message() {
-  return <Messages />;
+export default async function Message() {
+  const supabase = createServerComponentClient({ cookies});
+  let userId:number
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/");
+  }
+  else{
+    const{data:userDetails,error}=await supabaseClient.from("SupabaseUsers").select("id").eq("authId",user.id).single();
+    if(userDetails){
+      userId=userDetails.id
+    }
+    else{
+      redirect("/")
+    }
+  }
+  return <Messages hostUserId={userId}/>;
 }
--- src/components/message/UserLink.tsx
+++ src/components/message/UserLink.tsx
@@ -4,17 +4,25 @@ import { useSearchParams } from "next/navigation"
 import profile1 from "../../../public/images/profile1.svg";
 import doubleTick from "../../../public/images/tick.svg";
 import blueTick from "../../../public/images/blueTick.svg";
-import { supabaseClient } from "@/lib/supabaseClient";
-import { useEffect, useState } from "react";
-import { handleClientScriptLoad } from "next/script";
+import { Dispatch, SetStateAction, useEffect} from "react";
+import moment from "moment";
 
-interface UserLinkProps {
+interface ConvoLinkProps {
   user: {
-    id: string,
-    Name: string,
-    profileUrl: string
+    id:number,
+    profileUrl:string,
+    Name:string,
+    latestMessage:{
+       senderId:number,
+       receiverId:number,
+       created_at:string,
+       content:string,
+       isRead:boolean
+    }|null,
+    pendingCount:number
   },
-  hostUserId: string
+  hostUserId: number|undefined,
+  setShowChat:Dispatch<SetStateAction<boolean>>
 }
 
 interface RealtimeEvent {
@@ -23,96 +31,24 @@ interface RealtimeEvent {
   new: any;
   schema: String;
 }
-const UserLink: React.FC<UserLinkProps> = ({ user, hostUserId }) => {
-  const searchParams = useSearchParams()
-  const [latestMessage, setLatestMessage] = useState<{
-    content: string,
-    isRead: boolean,
-    senderId: string
-  } | null>(null)
-  const [pendingCount, setPendingCount] = useState<number>(0)
+const UserLink: React.FC<ConvoLinkProps> = ({ user, hostUserId,setShowChat }) => {
+  const searchParams = useSearchParams();
 
-  const getLatestMessage = async () => {
-    try {
-      const { data, error } = await supabaseClient
-        .from("SupabaseMessages")
-        .select("*")
-        .or(
-          `and(senderId.eq.${hostUserId},receiverId.eq.${user.id}),and(senderId.eq.${user.id},receiverId.eq.${hostUserId})`
-        )
-        .order("created_at", { ascending: false }).limit(1);
-      if (data) {
-        if (data[0]) {
-          setLatestMessage(data[0])
-        }
-      }
-      else if (error) {
-        console.log(error)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  const getPendingCount = async () => {
-    try {
-      const { data, error } = await supabaseClient
-        .from("SupabaseMessages")
-        .select("*")
-        .or(
-          `and(senderId.eq.${user.id},receiverId.eq.${hostUserId})`
-        ).eq("isRead", false)
-      if (data) {
-        setPendingCount(data.length)
-      }
-      else if (error) {
-        console.log(error)
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  useEffect(()=>{
 
-  const handleNewMessageSubcription=(payload:any)=>{
-    const message=payload.new
-    const isMessegeByUser=(message.senderId==hostUserId&&message.receiverId==user.id)||(message.receiverId==hostUserId&&message.senderId==user.id);
-    if(isMessegeByUser){
-      setLatestMessage(message)
-    }
-    const isMessageForhost=message.senderId==user.id&&message.receiverId==hostUserId;
-    if(isMessageForhost){
-      if(payload.eventType=="INSERT"){
-        if(!message.isRead){
-          setPendingCount((pendingCount)=>pendingCount+1)
-        }
-      }
-      else if(payload.eventType=="UPDATE"){
-           const oldMessage=payload.old;
-           if(!oldMessage.isRead&&message.isRead){
-           setPendingCount(pendingCount=>pendingCount-1)
-           }
-      }
-    }
-  }
-  useEffect(() => {
-    getLatestMessage()
-    getPendingCount()
+  },[user])
 
-
-    const channels = supabaseClient.channel(`new-message-event-${user.id}`)
-      .on(
-        'postgres_changes',
-        { event: '*', schema: 'public', table: 'SupabaseMessages' },
-        handleNewMessageSubcription
-      )
-      .subscribe()
-  }, [user])
-
+  
+  
   return (
     <Link
       href={`/message?id=${user.id}`}
-      className={`grid flex-row grid-cols-[60px_auto] w-full mt-2.5 rounded-[10px] p-[10px] ${searchParams.get("id") == user.id ? "bg-[#2cbfca22]" : ""
+      className={`grid flex-row grid-cols-[60px_auto] w-full mt-2.5 rounded-[10px] p-[10px] ${searchParams.get("id") == user.id+"" ? "bg-[#2cbfca22]" : ""
         }`}
       key={user.id}
+      onClick={()=>{setTimeout(() => {
+        setShowChat(true)
+      },1000);}}
     >
       {" "}
       {/*check if this is the active convo*/}
@@ -125,29 +61,29 @@ const UserLink: React.FC<UserLinkProps> = ({ user, hostUserId }) => {
           <p className="text-lg font-medium text-[#283030]">
             {user?.Name}
           </p>
-          {latestMessage && <p
-            className={`text-xs font-normal ${pendingCount > 0 ? "text-[#2cbfca]" : "text-[#6c6c6c]"
+          {user.latestMessage && <p
+            className={`text-xs font-normal ${user.pendingCount > 0 ? "text-[#2cbfca]" : "text-[#6c6c6c]"
               }`}
           >
-            {"9.00"}
-          </p>}
+            {moment(user.latestMessage.created_at).format("HH:mm")}
+          </p>} 
           {/*apply the class if there are any pending messages*/}
         </div>
-        {latestMessage && <div className="flex justify-between align-center">
+        {user.latestMessage && <div className="flex justify-between align-center">
           <div className="flex flex-row gap-x-[5px]">
-            {latestMessage?.senderId == hostUserId && !latestMessage?.isRead && <Image src={doubleTick} alt="sent" />}{" "}
+            {user.latestMessage?.senderId == hostUserId && !user.latestMessage?.isRead && <Image src={doubleTick} alt="sent" />}{" "}
             {/*staus of last message to be checked*/}
-            {latestMessage?.senderId == hostUserId && latestMessage?.isRead && <Image src={blueTick} alt="read" />}
+            {user.latestMessage?.senderId == hostUserId && user.latestMessage?.isRead && <Image src={blueTick} alt="read" />}
             {/*status of last message to be checked*/}
             <p className="text-sm font-normal text-[#283030aa]">
-              {latestMessage?.content.length > 18 ? latestMessage.content.substring(0, 15) + "..." : latestMessage.content}
+              {user.latestMessage?.content.length > 18 ? user.latestMessage.content.substring(0, 15) + "..." : user.latestMessage.content}
             </p>
             {/* get the latest message*/}
           </div>
           <div className="flex justiy-center align-center">
-            {pendingCount > 0 && (
+            {user.pendingCount > 0 && (
               <p className="bg-[#2cbfca] text-sm font-normal text-white h-5 w-5 text-center rounded-[50%]">
-                {pendingCount}
+                {user.pendingCount}
               </p>
             )}{" "}
             {/*check if there are pending messages and its count*/}
